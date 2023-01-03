5f4bec37c8c7cb9a53714b7dbae067851bfc90c3
Taja Wasilewicz <116358644+wastaja@users.noreply.github.com>
2023-01-03 18:14:57 +0100

    Add files via upload

---
+++ quiz-game/main.py
@@ -0,0 +1,39 @@
+print ('Welcome to the quize game!')
+
+playing = input('Do you want to play? ')
+
+if playing != 'yes':
+    quit()
+
+print ("Okay! Let's play :)")
+
+answer = input("What does CPU stand for? ")
+
+if answer == 'central processing unit':
+    print('Correct! ')
+else:
+    print('Incorrect!')
+
+
+answer = input("What does GPU dtand for? ")
+
+if answer == 'graphics processing unit':
+    print('Correct! ')
+else:
+    print('Incorrect!')
+
+
+answer = input("What does RAM stand for? ")
+
+if answer == 'random accesss memory':
+    print('Correct! ')
+else:
+    print('Incorrect!')
+
+
+answer = input("What does PSU stand for? ")
+
+if answer == 'power supply unit':
+    print('Correct! ')
+else:
+    print('Incorrect!')
